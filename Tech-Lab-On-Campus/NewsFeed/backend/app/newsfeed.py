7a0ca77fd4a98be18eab7ee7ab9ffe3a931d1f12
rvallesmar <roger.v.383@gmail.com>
2025-02-20 19:39:53 -0700

    Update newsfeed.py
    completed get_all_news() and get_featured_news() functions, as well as adding the __turn_to_article() private function

--- Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
+++ Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
@@ -2,6 +2,8 @@
 
 from dataclasses import dataclass
 from datetime import datetime
+from app.utils.redis import REDIS_CLIENT
+from operator import attrgetter
 
 
 @dataclass
@@ -21,11 +23,25 @@ def get_all_news() -> list[Article]:
     # 1. Use Redis client to fetch all articles
     # 2. Format the data into articles
     # 3. Return the as a list of articles sorted by most recent
-    return []
+    full_articles = REDIS_CLIENT.get_entry('all_articles')
+    final_lst = []
+
+    for article in full_articles:
+        final_lst.append(__turn_to_article(article))
+    
+    return final_lst
 
 
 def get_featured_news() -> Article | None:
     """Get the featured news article from the datastore."""
     # 1. Get all the articles
     # 2. Select and return the featured article
-    return None
+    return sorted(get_all_news(), key= attrgetter('publish_date'), reverse=True)
+
+def __turn_to_article(data):
+    return Article(author=data['author'],
+                   title=data['title'],
+                   body=data['text'],
+                   publish_date= datetime.fromisoformat(data['published']),
+                   image_url=data['thread']['main_image'],
+                   url=data['url'])
